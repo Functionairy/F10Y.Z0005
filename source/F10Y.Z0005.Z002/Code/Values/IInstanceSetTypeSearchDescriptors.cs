4f40701935d91cdf7cd793b79a78459617cf1ca0
davidcoats <david@getsafetycone.com>
2026-04-07 12:28:52 -0700

    Interim commit.

--- source/F10Y.Z0005.Z002/Code/Values/IInstanceSetTypeSearchDescriptors.cs
+++ source/F10Y.Z0005.Z002/Code/Values/IInstanceSetTypeSearchDescriptors.cs
@@ -8,11 +8,15 @@ namespace F10Y.Z0005.Z002
     [ValuesMarker]
     public partial interface IInstanceSetTypeSearchDescriptors
     {
-        public InstanceSetTypeSearchDescriptor_Name Find_CatchAllLibraries => Instances.InstanceSetTypeSearchDescriptorOperator.From(
+        InstanceSetTypeSearchDescriptor_Name Find_CatchAllLibraries => Instances.InstanceSetTypeSearchDescriptorOperator.From(
             Instances.InstanceSetTypeNames.CatchAll_Library,
             Instances.EqualityOperationDescriptors.Simple);
 
-        public InstanceSetTypeSearchDescriptor_Name Find_FoundationLibraries => Instances.InstanceSetTypeSearchDescriptorOperator.From(
+        InstanceSetTypeSearchDescriptor_Name Find_EntryPoint_Executable => Instances.InstanceSetTypeSearchDescriptorOperator.From(
+            Instances.InstanceSetTypeNames.EntryPoint_Executable,
+            Instances.EqualityOperationDescriptors.Simple);
+
+        InstanceSetTypeSearchDescriptor_Name Find_FoundationLibraries => Instances.InstanceSetTypeSearchDescriptorOperator.From(
             Instances.InstanceSetTypeNames.Foundation_Library,
             Instances.EqualityOperationDescriptors.Simple);
     }
